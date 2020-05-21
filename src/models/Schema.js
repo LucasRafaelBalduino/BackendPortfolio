1b31f1b85f02443c585128e65590387c0405bfcb
lucasbalduino <lucas.balduino@nexenergy.com.br>
2020-05-21 09:51:28 -0300

    update

--- src/models/Schema.js
+++ src/models/Schema.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const { BASE_UPLOAD } = process.env;
 
 const ProjectSchema = new mongoose.Schema({
   name: String,
   image: String,
   link: String,
   description: String,
+  url: String,
   stack: [String],
 }, {
   toJSON: {
@@ -12,6 +17,9 @@ const ProjectSchema = new mongoose.Schema({
   },
 });
 
-ProjectSchema.virtual('image_url').get(() => `${process.BASE_UPLOAD}/files/${this.image}`);
+ProjectSchema.virtual('image_url').get(function () {
+  return `${BASE_UPLOAD}/files/${this.image}`;
+});
+
 
 module.exports = mongoose.model('Project', ProjectSchema);
